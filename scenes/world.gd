a771f76708df485d8155909b8ab2729946353336
Joel Paul <joel.v.paul@outlook.com>
2026-07-01 20:54:21 +1000

    MakerTech: First Cubes

---
+++ scenes/world.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+
+@export var world_size := Vector3(16, 16, 16)
+@export_range(-1, 1) var cut_off: float = 0.5
+@onready var default_cube: CSGBox3D = $DefaultCube
+
+
+func _ready() -> void:
+	var random_generator := FastNoiseLite.new()
+	for x in range(world_size.x):
+		for z in range(world_size.z):
+			for y in range(world_size.y):
+				var random = random_generator.get_noise_3d(x, y, z)
+				if random > cut_off:
+					var new_cube: CSGBox3D = default_cube.duplicate()
+					new_cube.position = Vector3(x, y, z)
+					add_child(new_cube)
+	remove_child(default_cube)
