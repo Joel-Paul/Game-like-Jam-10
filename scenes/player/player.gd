93cb0316fa6b696fd57e3d7904710762c52ebc19
Joel Paul <joel.v.paul@outlook.com>
2026-07-04 12:10:14 +1000

    Improve controls

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const FLY_SPEED = 24.0
 const JUMP_VELOCITY = 4.5
+const SPRINT_MULTIPLIER = 2.0
 
 @export var mouse_sensitivity: float = 0.01  # radians/pixel
 
@@ -22,16 +23,20 @@ func _physics_process(delta: float) -> void:
 			velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	if Input.is_action_just_pressed("fly"):
 		flying = !flying
+	
+	var speed_multiplier: float = 1.0
+	if Input.is_action_pressed("sprint"):
+		speed_multiplier = SPRINT_MULTIPLIER
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (eye_camera.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir := Input.get_vector("leftward", "rightward", "forward", "backward")
+	var direction := (eye_camera.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized() * speed_multiplier
 	if direction:
 		if flying:
 			velocity = direction * FLY_SPEED
@@ -42,8 +47,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	if Input.is_action_pressed("ui_accept") and flying:
+	if Input.is_action_pressed("jump") and flying:
 		velocity.y = FLY_SPEED
+	if Input.is_action_pressed("crouch") and flying:
+		velocity.y = -FLY_SPEED
 
 	move_and_slide()
 
